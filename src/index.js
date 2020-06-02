01909d2cbbcd56e0fc2739a1d8590c5f39124d04
cdramirezh <cdramirezh@unal.edu.co>
2020-06-02 13:04:56 -0500

    Components part 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Importo bootstrap antes de index para que index sobreescriba las clases custom que le meto a Bootstrap
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
