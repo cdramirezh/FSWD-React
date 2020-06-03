f8671da8ab331c247d11789651d39d4bfe32d70a
cdramirezh <cdramirezh@unal.edu.co>
2020-06-03 01:01:19 -0500

    Assignment 2. MenuComponent sin método innecesario

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -22,24 +22,6 @@ class Menu extends Component {
       selectedDish: dish});
   }
 
-  renderDish(dish) {
-    if (dish != null){
-      return(
-        <Card>
-          <CardImg width='100%' src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      )
-    }else{
-      return(
-        <div></div>
-      );
-    }
-  }
-
   render() {
 
     const menu = this.props.dishes.map( (dish) => {
