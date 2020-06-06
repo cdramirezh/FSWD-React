917dffa511c165dc9c7e76e1c45df38dfabaceba
cdramirezh <cdramirezh@unal.edu.co>
2020-06-05 23:39:43 -0500

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; //Importo bootstrap antes de index para que index sobreescriba las clases custom que le meto a Bootstrap
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
