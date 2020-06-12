f0006bda0c1f1b56aa7917e72ac46a8fcd6aeebd
cdramirezh <cdramirezh@unal.edu.co>
2020-06-11 21:10:25 -0500

    Redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -15,7 +15,7 @@ function RenderDish({dish}) {
   );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
 
   const commentList = comments.map( (comment) => {
     return(
@@ -41,7 +41,10 @@ function RenderComments({comments}) {
         <ul className="list-unstyled">
           {commentList}
         </ul>
-        <CommentForm />
+        <CommentForm 
+          dishId={dishId}
+          addComment={addComment}
+        />
       </div>
     );
   }else {
@@ -72,7 +75,11 @@ const DishDetail  = (props) => {
           </div>
           <div className='col-12 col-md-5 m-1'>
             <h4>Comments</h4>
-            <RenderComments comments={props.comments} />
+            <RenderComments 
+              comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id}
+            />
           </div>
         </div>
       </div>
@@ -109,8 +116,9 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values){
-    console.log('Current State is: ' + JSON.stringify(values))
-    alert('Current State is: ' + JSON.stringify(values))
+    this.toggleModal
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    // alert('Current State is: ' + JSON.stringify(values))
   }
 
   render(){
@@ -133,7 +141,6 @@ class CommentForm extends Component {
                     <option>3</option>
                     <option>4</option>
                     <option>5</option>
-                    <option>6</option>
                   </Control.select>
                 </Col>
               </Row>
@@ -164,11 +171,11 @@ class CommentForm extends Component {
               </Row>
               <Row className='form-group'>
                 <Col>
-                  <Label htmlFor="Modalcomment ">Comment</Label>
+                  <Label htmlFor="comment ">Comment</Label>
                   <Control.textarea 
-                    model=".modalcomment" 
-                    id="modalcomment" 
-                    name="modalcomment" 
+                    model=".comment" 
+                    id="comment" 
+                    name="comment" 
                     rows="6" 
                     className="form-control"
                   />
