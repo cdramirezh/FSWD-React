2be9ecdd8963c8af82ae68e55f7828cecbeedf94
cdramirezh <cdramirezh@unal.edu.co>
2020-06-13 19:01:55 -0500

    React Redux form revisited. Hacer que el form resetee el estado cada vez que se haya enviado. Usar la libreria react redux form para autogestionar el state

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -12,7 +14,12 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            //metera las reducer functions y mete la informacion en el estado, no tengo que hacer las reducers function
+            //Incluso si recargo la page, el form se mantiene, el initialFeedback me permite reset
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
