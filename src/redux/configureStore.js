8e321a1ddc97365543d5b61a7d479a595051bb4b
cdramirezh <cdramirezh@unal.edu.co>
2020-06-28 16:13:02 -0500

    combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     
     const store = createStore(
-        Reducer,
-        initialState
+        //Especificar como se combinaran los reducers
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
